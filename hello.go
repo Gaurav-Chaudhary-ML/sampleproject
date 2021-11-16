b20387d76614bc800bda5fec3b0563c7ed39527a
gaurav-dot-chaudhary <78791903+gaurav-dot-chaudhary@users.noreply.github.com>
2021-11-16 16:00:48 +0530

    initial commit

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+//https://docs.github.com/en/actions/learn-github-actions/workflow-syntax-for-github-actions#example-using-a-single-event
